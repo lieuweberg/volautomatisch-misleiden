ff7aeedf033748dc150e6f467d23ee666c181363
lieuweberg <36763108+lieuweberg@users.noreply.github.com>
2022-01-15 15:19:22 +0100

    famous first words: 'dat het volledig om'

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import csv
 
-import numpy as np
 import keras
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -34,21 +33,13 @@ def ints_to_string(ints):
     return tf.strings.reduce_join(layer(ints)).numpy().decode("utf-8")
 
 dataset = tf.data.Dataset.from_tensor_slices(raw_data)
-# for single_int in dataset.take(20):
-#     print(ints_to_string(single_int))
 sequences = dataset.batch(SEQUENCE_LENGTH+1, drop_remainder=True)
-
-# for seq in sequences.take(3):
-#     print(ints_to_string(seq))
 
 def map_func(seq):
     # All but the last, all but the first. This serves as input and target for predictions.
     return seq[:-1], seq[-1]
 
 dataset = sequences.map(map_func)
-# for input, target in dataset.take(3):
-#     print("Input :", ints_to_string(input))
-#     print("Target:", ints_to_string(target))
 dataset = dataset.shuffle(10000).batch(BATCH_SIZE, drop_remainder=True).prefetch(-1)
 
 vocab_length = len(vocab)
@@ -65,56 +56,6 @@ model.add(layers.Dropout(0.1))
 model.add(layers.Dense(SEQUENCE_LENGTH))
 model.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
 
-# class TextModel(keras.Model):
-#     def __init__(self, vocab_length, output_dimension, units):
-#         super().__init__(self)
-#         self.embedding = layers.Embedding(vocab_length, output_dimension)
-#         self.lstm = layers.LSTM(units, return_sequences=True, return_state=True)
-#         self.dense = layers.Dense(vocab_length)
-
-#     def call(self, input, state=None):
-#         output = self.embedding(input)
-#         if state == None:
-#             state = self.lstm.get_initial_state(input)
-#         output, state = self.lstm(output, initial_state=state)
-#         output = self.dense(input)
-
-#         return output, state
-
-# model = TextModel(vocab_length, 256, 512)
-# model.compile(loss="sparse_categorical_crossentropy")
-# model.build((None, 256))
-
-# model.summary()
-
-
-# class MyModel(tf.keras.Model):
-
-#     def __init__(self, vocab_size, embedding_dim, rnn_units):
-#         super().__init__(self)
-#         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-#         self.gru = tf.keras.layers.GRU(rnn_units,
-#                                        return_sequences=True,
-#                                        return_state=True)
-#         self.dense = tf.keras.layers.Dense(vocab_size)
-
-#     def call(self, inputs, states=None, return_state=False, training=False):
-#         x = inputs
-#         x = self.embedding(x, training=training)
-#         if states is None:
-#             states = self.gru.get_initial_state(x)
-#         x, states = self.gru(x, initial_state=states, training=training)
-#         x = self.dense(x, training=training)
-
-#         if return_state:
-#             return x, states
-#         else:
-#             return x
-
-
-# model = MyModel(vocab_size=vocab_length, embedding_dim=256, rnn_units=512)
-# model.compile(optimizer='adam', loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True))
-
 print(f"Model made, fitting model with {EPOCHS} epochs...")
 
 model.fit(dataset, epochs=EPOCHS)
@@ -122,9 +63,9 @@ model.fit(dataset, epochs=EPOCHS)
 print("Model fitted, generating some text...")
 
 # for input_example_batch, target_example_batch in dataset.take(1):
-input_string = "hallo, mijn naam is Karin en ik ben naar Nederland gefietst omdat ik het hier mooi vindt. Vervolgens"
+input_string = "hallo, mijn naam is Karin en ik ben naar Nederland gefietst omdat ik het hier mooi vind. Vervolgens"
 result = ""
-for _ in range(20):
+for _ in range(200):
     tensor = string_to_ints(tf.strings.unicode_split(input_string[len(result):] + result, input_encoding="UTF-8"))
     tensor = tf.expand_dims(tensor, 0)
     prediction = model(tensor)
@@ -135,10 +76,3 @@ for _ in range(20):
     result += ints_to_string(index)
 
 print(input_string + result)
-
-# example_batch_loss = loss(target_example_batch, example_batch_predictions)
-# mean_loss = example_batch_loss.numpy().mean()
-# print("Prediction shape: ", example_batch_predictions.shape,
-#       " # (batch_size, sequence_length, vocab_length)")
-# print("Mean loss:        ", mean_loss)
-# print(tf.exp(mean_loss).numpy())
